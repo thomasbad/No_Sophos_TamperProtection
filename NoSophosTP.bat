0cba74ceffbdee4da0c64cf83f991e2377d585e7
mkaa <20796385+thomasbad@users.noreply.github.com>
2023-05-17 15:55:13 +0800

    Update to let user input specific drive letter

--- NoSophosTP.bat
+++ NoSophosTP.bat
@@ -3,7 +3,27 @@ CLS
 Title No Sophos TP
 Color 17
 
+:MENU2
+ECHO ..............................................................................................
+ECHO .
+ECHO      88b 88  dP"Yb      .dP"Y8  dP"Yb  88""Yb 88  88  dP"Yb  .dP"Y8     888888 88""Yb 
+ECHO      88Yb88 dP   Yb     `Ybo." dP   Yb 88__dP 88  88 dP   Yb `Ybo."       88   88__dP 
+ECHO      88 Y88 Yb   dP     o.`Y8b Yb   dP 88"""  888888 Yb   dP o.`Y8b       88   88"""  
+ECHO      88  Y8  YbodP      8bodP'  YbodP  88     88  88  YbodP  8bodP'       88   88     
+ECHO .
+ECHO ..............................................................................................
+ECHO .
+ECHO ...............................................
+ECHO Remember You Should run this as Admin under WIN PE
+ECHO This process needs to do twice to ensure it is totally loaded by SOPHOS
+ECHO Now Please Input the Drive Letter of the system you need to modify
+ECHO ...............................................
+SET DRIVELETTER=
+SET /P DRIVELETTER=Please select a number then press ENTER:
+
+
 :MENU
+CLS
 ECHO ..............................................................................................
 ECHO .
 ECHO      88b 88  dP"Yb      .dP"Y8  dP"Yb  88""Yb 88  88  dP"Yb  .dP"Y8     888888 88""Yb 
@@ -38,10 +58,11 @@ ECHO ======PRESS ANY KEY TO CONTINUE======
 PAUSE > NUL
 GOTO MENU
 
+
 :DISTP
 CLS
-reg load HKLM\TEMPSYSTEM C:\Windows\System32\config\SYSTEM
-reg load HKLM\TEMPSOFTWARE C:\Windows\System32\config\SOFTWARE
+reg load HKLM\TEMPSYSTEM %DRIVELETTER%:\Windows\System32\config\SYSTEM
+reg load HKLM\TEMPSOFTWARE %DRIVELETTER%:\Windows\System32\config\SOFTWARE
 reg add "HKEY_LOCAL_MACHINE\TEMPSYSTEM\ControlSet001\Services\Sophos MCS Agent" /v Start /t REG_DWORD /d 0x00000004 /f
 reg add "HKEY_LOCAL_MACHINE\TEMPSYSTEM\ControlSet001\Services\Sophos Endpoint Defense\TamperProtection\Config" /v SAVEnabled /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\TEMPSYSTEM\ControlSet001\Services\Sophos Endpoint Defense\TamperProtection\Config" /v SEDEnabled /t REG_DWORD /d 0 /f
@@ -61,8 +82,8 @@ EXIT
 
 :ENTP
 CLS
-reg load HKLM\TEMPSYSTEM C:\Windows\System32\config\SYSTEM
-reg load HKLM\TEMPSOFTWARE C:\Windows\System32\config\SOFTWARE
+reg load HKLM\TEMPSYSTEM %DRIVELETTER%:\Windows\System32\config\SYSTEM
+reg load HKLM\TEMPSOFTWARE %DRIVELETTER%:\Windows\System32\config\SOFTWARE
 reg add "HKEY_LOCAL_MACHINE\TEMPSYSTEM\ControlSet001\Services\Sophos MCS Agent" /v Start /t REG_DWORD /d 0x00000002 /f
 reg add "HKEY_LOCAL_MACHINE\TEMPSYSTEM\ControlSet001\Services\Sophos Endpoint Defense\TamperProtection\Config" /v SAVEnabled /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\TEMPSYSTEM\ControlSet001\Services\Sophos Endpoint Defense\TamperProtection\Config" /v SEDEnabled /t REG_DWORD /d 1 /f
